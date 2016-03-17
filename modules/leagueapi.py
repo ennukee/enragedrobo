fb4b5fb8c1492bc985b8d27817c881af8fae639d
Dylan Bowers <Dylan Bowers>
2016-03-16 22:54:55 -0400

    ..

--- modules/leagueapi.py
+++ modules/leagueapi.py
@@ -12,9 +12,8 @@ import random # Random (RNG)
 import checks # Ensures various predefined conditions are met
 from utils.ResponseChecker import ResponseChecker # Checks HTTP response codes
 from utils.QueueType import QueueType # Converts queue IDs to names
-from utils.MapNames import MapNames	# Converts map IDs to names
+from utils.MapNames import MapNames # Converts map IDs to names
 from utils.Champion import * # Various champion data related operations
-
 
 # - - - - - - - - - - - - - - - - - - - - - - #
 # - - League of Legends API Usage section - - #
@@ -22,40 +21,40 @@ from utils.Champion import * # Various champion data related operations
 # - - - - - - - - - - - - - - - - - - - - - - #
 
 class LeagueAPI:
-	def __init__(self, bot):
-		self.bot = bot
-		self.riot_base = "https://na.api.pvp.net"
+  def __init__(self, bot):
+    self.bot = bot
+    self.riot_base = "https://na.api.pvp.net"
 
-	@commands.command()
-	async def player(self, name : str):
-		"""League of Legends player data"""
-		# TODO: Incomplete (use other API queries to improve this)
-		url = self.riot_base + '/api/lol/na/v1.4/summoner/by-name/{0}'.format(name)
-		params = dict(
-			api_key=load_api_key('LoL')
-		)
-		resp = requests.get(url=url, params=params)
-		if(ResponseChecker.is_good(resp.status_code)):
-			data = json.loads(resp.text)[name]
-			msg = """\
-				  **Username**: {0}
-				  **ID**: {1}
-				  **Level**: {2}""".format(data['name'], data['id'], data['summonerLevel'])
-			await self.bot.say(textwrap.dedent(msg))
-		else:
-			await self.bot.say(ResponseChecker.meaning(resp.status_code))
+  @commands.command()
+  async def player(self, name : str):
+    """League of Legends player data"""
+    # TODO: Incomplete (use other API queries to improve this)
+    url = self.riot_base + '/api/lol/na/v1.4/summoner/by-name/{0}'.format(name)
+    params = dict(
+      api_key=load_api_key('LoL')
+    )
+    resp = requests.get(url=url, params=params)
+    if(ResponseChecker.is_good(resp.status_code)):
+      data = json.loads(resp.text)[name]
+      msg = """\
+          **Username**: {0}
+          **ID**: {1}
+          **Level**: {2}""".format(data['name'], data['id'], data['summonerLevel'])
+      await self.bot.say(textwrap.dedent(msg))
+    else:
+      await self.bot.say(ResponseChecker.meaning(resp.status_code))
 
-	@commands.command()
-	async def champion(self, name : str):
-		"""League of Legends champion data"""
-		# TODO: Incomplete
-		name = name.replace('\'','')
-		data = champion_stats_by_name(name.capitalize())
-		if(data):
-			# parse data
-			await self.bot.say(data)
-		else:
-			await self.bot.say("That is not a valid champion name\n(Note: do not include spaces)")
+  @commands.command()
+  async def champion(self, name : str):
+    """League of Legends champion data"""
+    # TODO: Incomplete
+    name = name.replace('\'','')
+    data = champion_stats_by_name(name.capitalize())
+    if(data):
+      # parse data
+      await self.bot.say(data)
+    else:
+      await self.bot.say("That is not a valid champion name\n(Note: do not include spaces)")
 
 def setup(bot):
-	bot.add_cog(LeagueAPI(bot))
+  bot.add_cog(LeagueAPI(bot))
